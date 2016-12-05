42cb70071e028cc4c1dd5f420cdee5748898ace0
Ferris <fynet86@gmail.com>
2016-12-06 00:35:22 +0800

    数据模型

--- web/blog/admin.py
+++ web/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blog.models import Host
+
+class HostAdmin(admin.ModelAdmin):
+    list_display = ['hostname', 'ip']
+
+admin.site.register(Host, HostAdmin)
--- web/blog/models.py
+++ web/blog/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=50)
+    ip = models.GenericIPAddressField()
